8b85e083f9ffe7ff6f17558e6aa46b757f68b073
Danpa_cho <danpa725@cau.ac.kr>
2023-07-12 00:02:28 +0900

    feat: add diary core type

---
+++ react-ts/src/interface/diary.ts
@@ -0,0 +1,8 @@
+export type Diary = {
+    id: string
+    date: Date
+    title: string
+    content: string
+    emotion: 'bad' | 'soso' | 'good' | 'great' | 'awesome'
+    weather: 'sunny' | 'cloud' | 'rain' | 'snow'
+}
